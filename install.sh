f29aebae8075c15976af2317544e228d9345e182
Kalashnikov Michael <49352386+m-v-kalashnikov@users.noreply.github.com>
2021-10-12 00:41:30 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,6 +13,41 @@
 
 set -e
 
-sudo apt install -y gcc git make build-essential
-git clone https://github.com/m-v-kalashnikov/setup-files.git
-cd setup-files && make ubuntu_all
+apt_install() {
+	sudo apt install -y gcc git make build-essential
+	git clone https://github.com/m-v-kalashnikov/setup-files.git
+	cd setup-files && make apt_all
+}
+
+termux_install() {
+	pkg install -y libllvm git make build-essential
+	git clone https://github.com/m-v-kalashnikov/setup-files.git
+	cd setup-files && make termux_all
+}
+
+main() {
+	# check if termux [termux_install & exit || pass]
+	case "$PREFIX" in
+    	*com.termux*) termux=true ;;
+    	*) termux=false ;;
+  	esac
+
+	if [ "$termux" != true ]; then
+		termux_install
+		exit 0
+	fi
+	
+	# check if apt exists [apt_install & exit || pass]
+	case "$(which apt)" in
+    	*apt*) apt=true ;;
+    	*) apt=false ;;
+  	esac
+	
+	f [ "$apt" != true ]; then
+		apt_install
+		exit 0
+	fi
+	
+	echo "This script wasn\'t designed for your system!"
+	exit 1
+}
