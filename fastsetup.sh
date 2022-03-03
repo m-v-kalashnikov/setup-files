623fbdad71aed925c2a4ca16e8d2f1c7b4f649ab
Michael Kalashnikov <kalashnikovsystem@gmail.com>
2022-03-04 01:22:45 +0200

    feat: add fastsetup

--- fastsetup.sh
+++ fastsetup.sh
@@ -33,7 +33,7 @@ install_docker() {
 
   updating_system
   install_apt ca-certificates curl gnupg lsb-release
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --yes --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
   echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
   updating_system
