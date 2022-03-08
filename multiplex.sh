385cf23d05a0738e1bf499400e9fe32143301c54
Michael Kalashnikov <kalashnikovsystem@gmail.com>
2022-03-09 01:35:57 +0200

    feat: add m.sh

--- multiplex.sh
+++ multiplex.sh
@@ -11,13 +11,11 @@ clear() {
 }
 
 run_in_pane() {
-  i=1
-  tmux new-window -t multiplex "$2"
+  i=0
 
   while [ $i -lt "$1" ]
   do
-    tmux split-window -t multiplex:2 "$2"
-    tmux select-layout -t multiplex:2 tiled
+    tmux new-window -t multiplex "$2"
 
     i=$((i+1))
   done
